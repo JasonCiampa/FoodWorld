3001a5b9d55ca8ba65fddb8b5ec5644f7d1ab866
JasonCiampa <jciampa@mail.endicott.edu>
2024-10-30 12:32:36 -0400

    Tilemap Enable/Disable Experiment
    I did a quick experiment to see how the game would be affected when the HUGE tilemap we currently have is enabled or disabled. When it is invisible and I try to enable it, the game actually freezes for a brief second because of how huge the tilemap is.
    
    I think we'll need to subdivide the map into several smaller and separate tilemaps. Each world should have its own tilemap, and I also think that even within those worlds there could be even further subdivided tile maps for better memory usage. We can enable/disable certain ones at certain points based on the Player's location.

--- food_world_game/scripts/game.gd
+++ food_world_game/scripts/game.gd
@@ -10,6 +10,8 @@ extends Node2D
 
 @onready var FUSION_MALICK_SALLY: FoodBuddyFusion = load("res://scenes/fusions/malick_sally.tscn").instantiate()
 
+@onready var ryans_tile_ma_p_bkg: TileMapLayer = $"Ryans TileMaP BKG"
+
 var food_citizen = load("res://scenes/blueprints/food_citizen.tscn").instantiate()
 
 #------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
@@ -89,6 +91,11 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("ability1"):
+		ryans_tile_ma_p_bkg.visible = true
+	if Input.is_action_just_pressed("ability2"):
+		ryans_tile_ma_p_bkg.visible = false
+	
 	enemies = get_tree().get_nodes_in_group("enemies")
 	food_citizens = get_tree().get_nodes_in_group("food_citizens")
 	interactables = get_tree().get_nodes_in_group("interactables")
